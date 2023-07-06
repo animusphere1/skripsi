0cb30ac902226109ae5e1280327e64e72f61313e
Fajar Bagus Widiarno <widiarnofajarbagus@gmail.com>
2023-07-06 18:09:54 +0700

    nothing

--- backend/routes/jeep_datas/all_data.js
+++ backend/routes/jeep_datas/all_data.js
@@ -1,9 +1,20 @@
+const { json } = require("body-parser");
 const express = require("express");
 
 const getAllDataRouter = express.Router();
 
 getAllDataRouter.get("/", (req, res) => {
-  res.send("getall");
+  let { data } = req.body;
+
+  let datas = JSON.parse(data);
+
+  if (typeof Array.isArray(datas)) {
+    const found = datas.find((element) => element["fajar"] == 1);
+
+    console.log(found);
+
+    res.status(200).json({ datas: found });
+  }
 });
 
 module.exports = { getAllDataRouter };
